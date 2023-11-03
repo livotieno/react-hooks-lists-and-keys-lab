cf65b98d1c7bcbda41306b0175ab7a13f5f603b8
livotieno <livio5261@gmail.com>
2023-11-03 09:23:21 +0300

    done

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,19 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map((project) => {
+          const { id, name, about, technologies } = project;
+          return (
+            <ProjectItem
+              key={id}
+              name={name}
+              about={about}
+              technologies={technologies}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
